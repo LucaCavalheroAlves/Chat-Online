55a75e6c1266aca01f3f05ad320cb2e0b0f13b5c
José Cláudio <jose.c.lima.sp@gmail.com>
2024-03-05 15:20:32 -0300

    Atualização do chatOnline

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-const express = require('express')
-const app = express()
+//Importação dos modulos/frameworks
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const path = require('path');
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
 
-app.listen(process.env.PORT || 3000)
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+io.on('connection', socket => {
+  console.log('Novo usuário conectado');
+
+  socket.on('chat message', (msg, nome) => {
+    io.emit('chat message', msg, nome);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Usuário desconectado');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
